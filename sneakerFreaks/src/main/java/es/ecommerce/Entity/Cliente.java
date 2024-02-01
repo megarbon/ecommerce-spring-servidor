7db2a3e0330c1fd2fcdd5e0c96832863fab85e64
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2024-02-01 11:11:52 +0100

    entitity change

--- sneakerFreaks/src/main/java/es/ecommerce/Entity/Cliente.java
+++ sneakerFreaks/src/main/java/es/ecommerce/Entity/Cliente.java
@@ -1,15 +1,19 @@
 package es.ecommerce.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table (name = "cliente")
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "nombre")
     private Long idCliente;
 
     private String nombre;
