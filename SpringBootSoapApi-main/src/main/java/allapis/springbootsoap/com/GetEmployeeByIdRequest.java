6f0a95dd3ff94a3787471e03426e223e5560e6e2
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2024-03-01 20:31:13 +0100

    soap API

---
+++ SpringBootSoapApi-main/src/main/java/allapis/springbootsoap/com/GetEmployeeByIdRequest.java
@@ -0,0 +1,56 @@
+
+package allapis.springbootsoap.com;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+/**
+ * <p>
+ * Java class for anonymous complex type.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="employeeId" type="{http://www.w3.org/2001/XMLSchema}long"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "employeeId"
+})
+@XmlRootElement(name = "getEmployeeByIdRequest")
+public class GetEmployeeByIdRequest {
+
+    protected long employeeId;
+
+    /**
+     * Gets the value of the employeeId property.
+     * 
+     */
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    /**
+     * Sets the value of the employeeId property.
+     * 
+     */
+    public void setEmployeeId(long value) {
+        this.employeeId = value;
+    }
+
+}
---
+++ SpringBootSoapApi-main/src/main/java/allapis/springbootsoap/com/package-info.java
@@ -0,0 +1,3 @@
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://com.springbootsoap.allapis", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package allapis.springbootsoap.com;
